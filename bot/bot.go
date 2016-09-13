7d15705ddf96a941e76d963c54d18ed21b3533af
Pablo de la Concepcion <pconcepcion@gmail.com>
2016-09-13 02:34:04 +0200

    Modified bot to work with commands (WIP)

--- bot/bot.go
+++ bot/bot.go
@@ -55,12 +55,53 @@ func authorizeBot(debug bool) *tgbotapi.BotAPI {
 
 }
 
+func handleMessage(m *tgbotapi.Message) string {
+	var response string = "Unknown command"
+	log.Infof("Bot command: %v, arguments %v", m.Command(), m.CommandArguments())
+	switch m.Command() {
+	// Dice expression
+	case "de":
+		diceExpression := m.CommandArguments()
+		dicesResult, err := roll(diceExpression)
+		if err != nil {
+			// TODO: handle the error gracefully
+			log.Error(err)
+		}
+		response = fmt.Sprintf("%s: %v -> %s", diceExpression, dicesResult.GetResults(), dicesResult)
+		// Basic dices
+	case "d2":
+		response = fmt.Sprintf("d2 -> %d", rpg.D2())
+	case "d4":
+		response = fmt.Sprintf("d4 -> %d", rpg.D4())
+	case "d6":
+		response = fmt.Sprintf("d6 -> %d", rpg.D6())
+	case "d8":
+		response = fmt.Sprintf("d8 -> %d", rpg.D8())
+	case "d10":
+		response = fmt.Sprintf("d10 -> %d", rpg.D10())
+	case "d12":
+		response = fmt.Sprintf("d12 -> %d", rpg.D12())
+	case "d20":
+		response = fmt.Sprintf("d20 -> %d", rpg.D20())
+	case "d100":
+		response = fmt.Sprintf("d100 -> %d", rpg.D100())
+	}
+	return response
+}
+
 // Run launches the bot, does the authorization process and starts to listen for messages
 func Run() {
 	// Athorize the bot with deboug mode
 	bot := authorizeBot(true)
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = 30
+
+	self, err := bot.GetMe()
+	if err != nil {
+		log.Error(err)
+	} else {
+		log.Infof("Bot info: %v", self)
+	}
 
 	updates, err := bot.GetUpdatesChan(u)
 	if err != nil {
@@ -73,13 +114,9 @@ func Run() {
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		diceExpression := update.Message.Text
-		dicesResult, err := roll(diceExpression)
-		if err != nil {
-			// TODO: handle the error gracefully
-			log.Error(err)
-		}
-		response := fmt.Sprintf("%s: %v -> %s", diceExpression, dicesResult.GetResults(), dicesResult)
+		log.Printf("---\n%+v\n---", update.Message)
+		log.Printf("---\n%+v\n---", update.Message.Chat)
+		response := handleMessage(update.Message)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
 		msg.ReplyToMessageID = update.Message.MessageID
 
